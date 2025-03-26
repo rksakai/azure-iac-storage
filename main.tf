7b59de5cbf9c2733c98ff1ff6e10bbc58271b3f3
Rodrigo Katsumoto Sakai <rodrigo.sakai@petrobras.com.br>
2025-03-26 18:54:33 -0300

    Alteração subscription ID

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
 provider "azurerm" {
   features {}
+  subscription_id      = "58629e9c-adb4-4830-a6db-f026096730fa"
+  tenant_id            = "8f383ce2-97da-44f5-aee2-11e166311c59"
+
 }
 
 resource "azurerm_resource_group" "example" {
@@ -8,7 +11,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "examplestoracc123" # O nome deve ser único
+  name                     = "stlivefiap" # O nome deve ser único
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier            = "Standard"
